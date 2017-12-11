b04d61d7ec7dfa0f551a79356076b1cb38e3abf9
Stuart Pittaway <stuart@binarycreations.co.uk>
2017-12-11 13:30:55 +0000

    Changed EEPROM update and write

--- Arduino-ESP8266-BMS-Controller/Arduino-ESP8266-BMS-Controller.ino
+++ Arduino-ESP8266-BMS-Controller/Arduino-ESP8266-BMS-Controller.ino
@@ -32,6 +32,7 @@ extern "C"
 
 #include <Wire.h>
 
+#include <CRC32.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiClient.h>
@@ -59,6 +60,8 @@ ESP8266WebServer server(80);
 #define read_error_counter 15
 #define read_bypass_enabled_state 16
 #define read_bypass_voltage_measurement 17
+
+#define EEPROM_storageSize 2048
 
 //Default i2c SLAVE address (used for auto provision of address)
 #define DEFAULT_SLAVE_ADDR 21
@@ -246,26 +249,6 @@ struct wifi_eeprom_settings {
 
 wifi_eeprom_settings myConfig_WIFI;
 
-uint32_t calculateCRC32(const uint8_t *data, size_t length)
-{
-  uint32_t crc = 0xffffffff;
-  while (length--) {
-    uint8_t c = *data++;
-    for (uint32_t i = 0x80; i > 0; i >>= 1) {
-      bool bit = crc & 0x80000000;
-      if (c & i) {
-        bit = !bit;
-      }
-      crc <<= 1;
-      if (bit) {
-        crc ^= 0x04c11db7;
-      }
-    }
-  }
-  return crc;
-}
-
-
 struct eeprom_settings {
   int a;
   int b;
@@ -277,24 +260,32 @@ eeprom_settings myConfig;
 #define EEPROM_WIFI_CHECKSUM_ADDRESS 0
 #define EEPROM_WIFI_CONFIG_ADDRESS EEPROM_WIFI_CHECKSUM_ADDRESS+sizeof(uint32_t)
 
-#define EEPROM_CHECKSUM_ADDRESS EEPROM_WIFI_CONFIG_ADDRESS+sizeof(wifi_eeprom_settings)
+#define EEPROM_CHECKSUM_ADDRESS 256
 #define EEPROM_CONFIG_ADDRESS EEPROM_CHECKSUM_ADDRESS+sizeof(uint32_t)
 
 
 void WriteConfigToEEPROM() {
+  uint32_t checksum = CRC32::calculate(&myConfig, sizeof(eeprom_settings));
+  EEPROM.begin(EEPROM_storageSize);
   EEPROM.put(EEPROM_CONFIG_ADDRESS, myConfig);
-  EEPROM.put(EEPROM_CHECKSUM_ADDRESS, calculateCRC32((uint8_t*)&myConfig, sizeof(eeprom_settings)));
+  EEPROM.put(EEPROM_CHECKSUM_ADDRESS, checksum);
+  EEPROM.end();
 }
 
 bool LoadConfigFromEEPROM() {
   eeprom_settings restoredConfig;
   uint32_t existingChecksum;
 
+  EEPROM.begin(EEPROM_storageSize);
   EEPROM.get(EEPROM_CONFIG_ADDRESS, restoredConfig);
   EEPROM.get(EEPROM_CHECKSUM_ADDRESS, existingChecksum);
+  EEPROM.end();
 
   // Calculate the checksum of an entire buffer at once.
-  uint32_t checksum = calculateCRC32((uint8_t*)&restoredConfig, sizeof(eeprom_settings));
+  uint32_t checksum = CRC32::calculate(&restoredConfig, sizeof(eeprom_settings));
+ 
+  Serial.println(checksum);
+  Serial.println(existingChecksum);
 
   if (checksum == existingChecksum) {
     //Clone the config into our global variable and return all OK
@@ -306,20 +297,32 @@ bool LoadConfigFromEEPROM() {
   return false;
 }
 
+
 void WriteWIFIConfigToEEPROM() {
+  uint32_t checksum = CRC32::calculate(&myConfig_WIFI, sizeof(wifi_eeprom_settings));
+
+  EEPROM.begin(EEPROM_storageSize);
   EEPROM.put(EEPROM_WIFI_CONFIG_ADDRESS, myConfig_WIFI);
-  EEPROM.put(EEPROM_WIFI_CHECKSUM_ADDRESS, calculateCRC32((uint8_t*)&myConfig_WIFI, sizeof(wifi_eeprom_settings)));
+  EEPROM.put(EEPROM_WIFI_CHECKSUM_ADDRESS, checksum);
+  EEPROM.end();
+
+  Serial.println(checksum);
 }
 
 bool LoadWIFIConfigFromEEPROM() {
   wifi_eeprom_settings restoredConfig;
   uint32_t existingChecksum;
 
+  EEPROM.begin(EEPROM_storageSize);
   EEPROM.get(EEPROM_WIFI_CONFIG_ADDRESS, restoredConfig);
   EEPROM.get(EEPROM_WIFI_CHECKSUM_ADDRESS, existingChecksum);
-
+  EEPROM.end();
+  
   // Calculate the checksum of an entire buffer at once.
-  uint32_t checksum = calculateCRC32((uint8_t*)&restoredConfig, sizeof(wifi_eeprom_settings));
+  uint32_t checksum = CRC32::calculate(&restoredConfig, sizeof(wifi_eeprom_settings));
+
+  Serial.println(checksum);
+  Serial.println(existingChecksum);
 
   if (checksum == existingChecksum) {
     //Clone the config into our global variable and return all OK
@@ -357,9 +360,9 @@ String htmlFooter() {
 }
 
 void handleRoot()
-{ 
+{
   String s;
-  s = htmlHeader()+"<h2>WiFi Setup</h2><p>Select local WIFI to connect to:</p>";
+  s = htmlHeader() + "<h2>WiFi Setup</h2><p>Select local WIFI to connect to:</p>";
   s += "<form autocomplete=\"off\" method=\"post\" enctype=\"application/x-www-form-urlencoded\" action=\"\\save\">";
   s += "<label for=\"ssid\">SSID:</label><select id=\"ssid\" name=\"ssid\">";
   s += networks;
@@ -373,16 +376,32 @@ void handleRoot()
 }
 
 void handleSave() {
-
+  String s;
   String ssid = server.arg("ssid");
   String password = server.arg("pass");
-  
-  String s;
-  s = htmlHeader()+"<p>WIFI settings saved, will reboot in a few seconds.</p><p>"+ssid+"</p><p>"+password+"</p>"+htmlFooter();  
-  sendHeaders();
-  server.send(200, "text/html", s);
 
-  //ESP.restart()
+  if ((ssid.length() <= sizeof(myConfig_WIFI.wifi_ssid)) && (password.length() <= sizeof(myConfig_WIFI.wifi_passphrase))) {
+    ssid.toCharArray(myConfig_WIFI.wifi_ssid, sizeof(myConfig_WIFI.wifi_ssid));
+    password.toCharArray(myConfig_WIFI.wifi_passphrase, sizeof(myConfig_WIFI.wifi_passphrase));
+
+    WriteWIFIConfigToEEPROM();
+
+    s = htmlHeader() + "<p>WIFI settings saved, will reboot in a few seconds.</p>" + htmlFooter();
+    sendHeaders();
+    server.send(200, "text/html", s);
+
+    for (int i=0;i<20;i++) {
+      delay(250);
+      yield();
+    }
+    ESP.restart();
+
+  } else {
+    s = htmlHeader() + "<p>WIFI settings too long.</p>" + htmlFooter();
+    sendHeaders();
+    server.send(200, "text/html", s);
+  }
+
 }
 
 void setupAccessPoint(void) {
@@ -418,7 +437,6 @@ void setupAccessPoint(void) {
       delay(1000);
     }
   }
-  Serial.println("mDNS responder started");
 
   server.on("/", HTTP_GET, handleRoot);
   server.on("/save", HTTP_POST, handleSave);
@@ -427,6 +445,7 @@ void setupAccessPoint(void) {
   server.begin();
   MDNS.addService("http", "tcp", 80);
 
+  Serial.println("Soft AP ready");
   while (1) {
     server.handleClient();
   }
@@ -439,6 +458,8 @@ void setup() {
   //D4 is LED
   pinMode(D4, OUTPUT);
   LED_OFF;
+
+  Serial.println("DIY BMS Startup");
 
   Wire.setTimeout(1000);  //1000ms timeout
   Wire.setClock(100000);  //100khz
@@ -453,7 +474,6 @@ void setup() {
   } else {
     //We are in initial power on mode (factory reset)
   }
-  
 
   if (LoadWIFIConfigFromEEPROM()) {
     Serial.println("WIFI Settings loaded from EEPROM");
